00dd45b1aa64576b1dcf0ffb433ddb90f45cddeb
Christian Howard <choward1491@gmail.com>
2019-06-27 21:06:42 -0500

    try adding a custom make_unique thing, based on the standard, so that this code can run on C++11

--- src/distr_utility/unique_handle.hpp
+++ src/distr_utility/unique_handle.hpp
@@ -14,6 +14,42 @@
 #include <stdio.h>
 #include <memory>
 
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace local {
+    template<class T> struct _Unique_if {
+        typedef std::unique_ptr<T> _Single_object;
+    };
+    
+    template<class T> struct _Unique_if<T[]> {
+        typedef std::unique_ptr<T[]> _Unknown_bound;
+    };
+    
+    template<class T, size_t N> struct _Unique_if<T[N]> {
+        typedef void _Known_bound;
+    };
+    
+    template<class T, class... Args>
+    typename _Unique_if<T>::_Single_object
+    make_unique(Args&&... args) {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+    
+    template<class T>
+    typename _Unique_if<T>::_Unknown_bound
+    make_unique(size_t n) {
+        typedef typename std::remove_extent<T>::type U;
+        return std::unique_ptr<T>(new U[n]());
+    }
+    
+    template<class T, class... Args>
+    typename _Unique_if<T>::_Known_bound
+    make_unique(Args&&...) = delete;
+}
+
 namespace util {
         
     template<typename T>
@@ -38,7 +74,7 @@ namespace util {
         // creation methods
         template <typename... Args>
         void create(Args&&... args){
-            ref = std::move(std::make_unique<T>(std::forward<Args>(args)...));
+            ref = std::move(local::make_unique<T>(std::forward<Args>(args)...));
         }
         
         // operators to allow use in containers
